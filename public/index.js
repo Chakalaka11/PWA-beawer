c8dcf9f84410ef2b322a2e2e5435a143ebd09dd9
Viktor Domin <viktor.domin@nice.com>
2021-02-14 13:50:19 +0200

    Added event

--- public/index.js
+++ public/index.js
@@ -1,15 +1,25 @@
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  console.log(deferredPrompt);
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
 // Service Worker registration
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("./serviceWorker.js")
-      .then(() => {
-        console.info("Service Worker installed successfully!");
-      })
-      .catch((error) => {
-        console.warn("Service Worker wasn't installed!");
-        console.warn("Error below");
-        console.warn(error);
-      })
-  }
-  else {
-    console.warn("Service Worker isn't supported")
-  }
+  navigator.serviceWorker.register("./serviceWorker.js")
+    .then(() => {
+      console.info("Service Worker installed successfully!");
+    })
+    .catch((error) => {
+      console.warn("Service Worker wasn't installed!");
+      console.warn("Error below");
+      console.warn(error);
+    })
+}
+else {
+  console.warn("Service Worker isn't supported")
+}
+
